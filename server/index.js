96228087f89bffc4ad5f7aa87b63937b8021fd32
GOMS <gomathiram338@gmail.com>
2024-11-24 15:21:03 +0530

    Resolved

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["http://localhost:3000", "http://localhost:3001","https://task-manager-1-adcp.onrender.com"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
